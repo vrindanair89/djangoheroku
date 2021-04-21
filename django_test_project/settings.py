2a97b388a9284d6e1d93ec08fd1daa5a2cb87d96
vrindanair89 <vrindanair89@gmail.com>
2021-04-21 13:06:08 +0530

    SECOND commit

--- django_test_project/settings.py
+++ django_test_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r)_h7(qc8+g8is+-1!!c!zo39xv*gkvtau3ffmz%1&&x+=^tru'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
