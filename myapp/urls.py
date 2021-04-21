6ac7b2be1c4bed3a5ce10edda2c976c4a6d131bc
vrindanair89 <vrindanair89@gmail.com>
2021-04-21 13:32:13 +0530

    sixth commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     path('test1',views.testfunc1,name='test1'),
     path('test2',views.testfunc2,name='test2'),
-    path('login',views.testfunc3,name='login')
+    path('',views.testfunc3,name='login')
 ]
